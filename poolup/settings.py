81f48879a654ff8bdc80259b653e229d2da9052b
whoamins <40am1ns@github.com>
2022-03-19 22:41:13 +0300

    token

--- poolup/settings.py
+++ poolup/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b1)h84%*2&sddo*f^v@@1)g07783tck*j9n_7u+z*e2lr(vq(6'
+# HAHA, NICE TRY BRO, TOKEN FROM PREVIOUS COMMIT ALREADY CHANGED
+SECRET_KEY = os.getenv('TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
