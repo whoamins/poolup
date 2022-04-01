765e4c797a6325e00a392fec3fa261b245898fa8
whoamins <ilsaf.nabiullin@whoamins.ru>
2022-04-01 22:02:56 +0300

    Fix bug with future questions displaying

--- pools/views.py
+++ pools/views.py
@@ -2,13 +2,16 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import View
+from django.utils import timezone
 
 from pools.models import Question, Choice
 
 
 class IndexView(View):
     def get(self, request):
-        questions_list = Question.objects.order_by('-pub_date')[:5]
+        questions_list = Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
         return render(request, 'pools/index.html', context={"questions_list": questions_list})
 
